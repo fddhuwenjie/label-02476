039992dea1d0f411d8288a1d9fa9436f22992351
胡文杰 <huwenjie@benzhi.io>
2026-06-10 11:51:57 +0800

    fix: 修复Dockerfile jar路径和MapperScan配置

--- backend/src/main/java/com/yuxuanlei/LibraryWebApplication.java
+++ backend/src/main/java/com/yuxuanlei/LibraryWebApplication.java
@@ -1,9 +1,11 @@
 package com.yuxuanlei;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yuxuanlei.mapper")
 public class LibraryWebApplication {
 
     public static void main(String[] args) {
